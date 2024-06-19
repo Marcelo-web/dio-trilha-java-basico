99845e2e60448cd7a19ec67a46b7634984a3e07b
Marcelo <marestdesenv@gmail.com>
2024-06-18 23:09:32 -0300

    estudando sobre métodos

---
+++ metodos/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * MÉTODOS
+         * 
+         * - São definidos no interior de classes (não existem métodos globais).
+         * - Representam uma ação específica tendo uma responsabilidade única.
+         * - São representados por verbos no infinitivo.
+         * - É facultativo ter parâmetros de entrada bem como um retorno. 
+         * - Quando não retornam um valor, possuem a palavra void no método.
+         * - Devem ser nomeados usando padrão camelCase.
+         * - Podem ser acessados de acordo com sua visilidade (dentro da própria classe, no pacote, em classes herdadas ou somente na própria classe).
+         * - O nome do método está vinculado a sua lógica. Por ex: o método validar, deve sempre retornar um booleano, para indicar se é válido ou não.
+         * - Se um método possui muitos parâmetros que podem ser representados por uma única abstração de um objeto, é interessante criar uma classe com todos os seus atributos. Por ex: Em um método gravarCliente que recebe vários parâmetros relacionados ao cadastro de um cliente, pode ser criada uma classe cliente com todos os atributos necessários. Desta forma,  posso definir um único parâmetro do tipo Cliente no método.
+         * 
+         * Aspectos relacionados:
+         * - Proposta do método => Qual a sua finalidade?
+         * - Tem parâmetros de entrada?
+         * - Retorno ou ausência de retorno?
+         * - Apresenta risco de exceções?
+         * - Visibilidade do método
+         */
+
+    }
+    
+    public static void abrirConexao() throws Exception {
+        // Toda lógica vem aqui
+    }
+
+    public static int somar(int n1, int n2) {
+        return n1 + n2;
+    }
+}
