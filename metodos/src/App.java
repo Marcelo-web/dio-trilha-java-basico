31b706868fb07e129e00fda0230925b90971dc07
Marcelo <marestdesenv@gmail.com>
2024-06-18 23:11:43 -0300

    fix - texto

--- metodos/src/App.java
+++ metodos/src/App.java
@@ -10,7 +10,7 @@ public class App {
          * - É facultativo ter parâmetros de entrada bem como um retorno. 
          * - Quando não retornam um valor, possuem a palavra void no método.
          * - Devem ser nomeados usando padrão camelCase.
-         * - Podem ser acessados de acordo com sua visilidade (dentro da própria classe, no pacote, em classes herdadas ou somente na própria classe).
+         * - Podem ser acessados de acordo com sua visibilidade (dentro da própria classe, no pacote, em classes herdadas ou somente na própria classe).
          * - O nome do método está vinculado a sua lógica. Por ex: o método validar, deve sempre retornar um booleano, para indicar se é válido ou não.
          * - Se um método possui muitos parâmetros que podem ser representados por uma única abstração de um objeto, é interessante criar uma classe com todos os seus atributos. Por ex: Em um método gravarCliente que recebe vários parâmetros relacionados ao cadastro de um cliente, pode ser criada uma classe cliente com todos os atributos necessários. Desta forma,  posso definir um único parâmetro do tipo Cliente no método.
          * 
